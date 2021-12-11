226b4ac1e7b16e97c8c39ae2656e25c1bf103dbd
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-10 17:08:48 -0800

    Added UserUtem componet

--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,14 +1,77 @@
-import React, { useffect, useState } from 'react'
-import { Avatar, useChatChannel } from 'stream-chat-react';
+import React, { useEffect, useState } from 'react'
+import { Avatar, useChatContext } from 'stream-chat-react';
 
 import { InviteIcon } from '../assets/InviteIcon';
 
+const ListContainer = ({ children }) => {
+  return (
+    <div className="user-list__container">
+      <div className="user-list__header">
+        <p>User</p>
+        <p>Invite</p>
+      </div>
+      {children}
+    </div>
+  )
+}
+
+const UserItem = ({ user }) => {
+  return (
+    <div className="user-item__wrapper">
+      <div className="user-item__name-wrapper">
+
+        {/* <Avatar image={user} name={user.fullName || user.id} size={32} />  */}
+
+        <Avatar />
+      </div>
+    </div>
+
+  )
+}
+
 const UserList = () => {
+  const { client } = useChatContext();
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [listEmpty, setListEmpty] = useState(false);
+
+  useEffect(() => {
+    const getUsers = async () => {
+  
+      if(loading) return;
+      
+      setLoading(true);
+
+      try {
+          const response = await client.queryUsers(
+            { id: { $ne: client.userID}},
+            { id: 1 },
+            { limit: 8 }
+          );
+
+          if (response.users.length){
+            setUsers(response.users)
+          } else {
+            setListEmpty(true);
+          }
+      } catch (error) {
+        console.log(error);
+      }
+      setLoading(false);
+    }
+
+    if(client) getUsers()
+  }, [])
 
   return (
-    <div>
+    <ListContainer>
+      {loading ? <div className="user-list__message">Loading users ...</div> :(
+        users?.map((user, i) => (
+            <UserItem />
+        ))
+      )}
       UserList
-    </div>
+    </ListContainer>
   )
 
 }
