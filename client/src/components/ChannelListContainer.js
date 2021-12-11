6e6c06b2517cf2f560f257f6959980724d5e070d
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-10 23:45:07 -0800

    Added responsive option to ChannelListContainer

--- client/src/components/ChannelListContainer.js
+++ client/src/components/ChannelListContainer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ChannelList, useChatContext } from "stream-chat-react";
 import Cookies from "universal-cookie";
 
@@ -29,18 +29,36 @@ const CompanyHeader = () => (
   </div>
 );
 
-const ChannelListContainer = ({ isCreating, setIsCreating, setCreateType, setIsEditing }) => {
+const customChannelTeamFilter = (channels) => {
+  return channels.filter((channel) => channel.type === "team");
+};
+
+const customChannelMessagingFilter = (channels) => {
+  return channels.filter((channel) => channel.type === "messaging");
+};
+
+const ChannelListContent = ({
+  isCreating,
+  setIsCreating,
+  setCreateType,
+  setIsEditing,
+  setToggleContainer,
+}) => {
+  const { client } = useChatContext();
+
   const logout = () => {
     cookies.remove("token");
-    cookies.remove('userId');
-    cookies.remove('username');
-    cookies.remove('fullName');
-    cookies.remove('avatarURL');
-    cookies.remove('hashedPassword');
-    cookies.remove('phoneNumber');
+    cookies.remove("userId");
+    cookies.remove("username");
+    cookies.remove("fullName");
+    cookies.remove("avatarURL");
+    cookies.remove("hashedPassword");
+    cookies.remove("phoneNumber");
 
     window.location.reload();
-  }
+  };
+
+  const filters = { members: { $in: [client.userID] } };
 
   return (
     <>
@@ -49,34 +67,34 @@ const ChannelListContainer = ({ isCreating, setIsCreating, setCreateType, setIsE
         <CompanyHeader />
         <ChannelSearch />
         <ChannelList
-          filters={{}}
-          channelRenderFilterFn={() => {}}
-          List={(listProps) => ( 
-            <TeamChannelList 
-                {...listProps} 
-                type="team" 
-                isCreating={isCreating}
-                setIsCreating={setIsCreating} 
-                setCreateType={setCreateType}
-                setIsEditing={setIsEditing}
-              />
-            )}
+          filters={filters}
+          channelRenderFilterFn={customChannelTeamFilter}
+          List={(listProps) => (
+            <TeamChannelList
+              {...listProps}
+              type="team"
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              setCreateType={setCreateType}
+              setIsEditing={setIsEditing}
+            />
+          )}
           Preview={(previewProps) => (
             <TeamChannelPreview {...previewProps} type="team" />
           )}
         />
 
         <ChannelList
-          filters={{}}
-          channelRenderFilterFn={() => {}}
+          filters={filters}
+          channelRenderFilterFn={customChannelMessagingFilter}
           List={(listProps) => (
-            <TeamChannelList 
-              {...listProps} 
-                type="messaging" 
-                isCreating={isCreating}
-                setIsCreating={setIsCreating} 
-                setCreateType={setCreateType}
-                setIsEditing={setIsEditing}
+            <TeamChannelList
+              {...listProps}
+              type="messaging"
+              isCreating={isCreating}
+              setIsCreating={setIsCreating}
+              setCreateType={setCreateType}
+              setIsEditing={setIsEditing}
             />
           )}
           Preview={(previewProps) => (
@@ -84,6 +102,50 @@ const ChannelListContainer = ({ isCreating, setIsCreating, setCreateType, setIsE
           )}
         />
       </div>
+    </>
+  );
+};
+
+const ChannelListContainer = ({
+  setCreateType,
+  setIsCreating,
+  setIsEditing,
+}) => {
+  const [toggleContainer, setToggleContainer] = useState(false);
+
+  return (
+    <>
+      <div className="channel-list__container">
+        <ChannelListContent
+          // isCreating={isCreating}
+          setIsCreating={setIsCreating}
+          setCreateType={setCreateType}
+          setIsEditing={setIsEditing}
+        />
+      </div>
+
+      <div
+        className="channel-list__container-responsive"
+        style={{
+          left: toggleContainer ? "0%" : "-89%",
+          backgroundColor: "#005fff",
+        }}
+      ></div>
+
+      <div
+        className="channel-list__container-toggle"
+        onClick={() =>
+          setToggleContainer((prevToggleContainer) => !prevToggleContainer)
+        }
+      ></div>
+
+      <ChannelListContent
+        // isCreating={isCreating}
+        setIsCreating={setIsCreating}
+        setCreateType={setCreateType}
+        setIsEditing={setIsEditing}
+        setToggleContainer={setToggleContainer}
+      />
     </>
   );
 };
--- client/src/components/UserList.js
+++ client/src/components/UserList.js
@@ -1,7 +1,7 @@
-import React, { useEffect, useState } from 'react'
-import { Avatar, useChatContext } from 'stream-chat-react';
+import React, { useEffect, useState } from "react";
+import { Avatar, useChatContext } from "stream-chat-react";
 
-import { InviteIcon } from '../assets/InviteIcon';
+import { InviteIcon } from "../assets/InviteIcon";
 
 const ListContainer = ({ children }) => {
   return (
@@ -12,35 +12,36 @@ const ListContainer = ({ children }) => {
       </div>
       {children}
     </div>
-  )
-}
+  );
+};
 
 const UserItem = ({ user, setSelectedUsers }) => {
-  const [selected, setSelected] = useState(false)
+  const [selected, setSelected] = useState(false);
 
   const handleSelect = () => {
-      if(selected) {
-        setSelectedUsers((prevUsers) => prevUsers.filter((prevUser) => prevUser !== user.id))
-      } else {
-        setSelectedUsers((prevUsers) => [...prevUsers, user.id])
-      }
+    if (selected) {
+      setSelectedUsers((prevUsers) =>
+        prevUsers.filter((prevUser) => prevUser !== user.id)
+      );
+    } else {
+      setSelectedUsers((prevUsers) => [...prevUsers, user.id]);
+    }
 
-      setSelected((prevSelected) => !prevSelected)
-  } 
+    setSelected((prevSelected) => !prevSelected);
+  };
 
   return (
     <div className="user-item__wrapper" onClick={handleSelect}>
       <div className="user-item__name-wrapper">
-        <Avatar image={user.image} name={user.fullName || user.id} size={32} /> 
+        <Avatar image={user.image} name={user.fullName || user.id} size={32} />
         <p className="user-item__name">{user.fullName || user.id}</p>
       </div>
       {selected ? <InviteIcon /> : <div className="user-item__invite-empty" />}
     </div>
+  );
+};
 
-  )
-}
-
-const UserList = ({setSelectedUsers}) => {
+const UserList = ({ setSelectedUsers }) => {
   const { client } = useChatContext();
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
@@ -49,43 +50,68 @@ const UserList = ({setSelectedUsers}) => {
 
   useEffect(() => {
     const getUsers = async () => {
-  
-      if(loading) return;
-      
+      if (loading) return;
+
       setLoading(true);
 
       try {
-          const response = await client.queryUsers(
-            { id: { $ne: client.userID}},
-            { id: 1 },
-            { limit: 8 }
-          );
+        const response = await client.queryUsers(
+          { id: { $ne: client.userID } },
+          { id: 1 },
+          { limit: 8 }
+        );
 
-          if (response.users.length){
-            setUsers(response.users)
-          } else {
-            setListEmpty(true);
-          }
+        if (response.users.length) {
+          setUsers(response.users);
+        } else {
+          setListEmpty(true);
+        }
       } catch (error) {
-        console.log(error);
+        setError(true);
       }
       setLoading(false);
-    }
+    };
 
-    if(client) getUsers()
-  }, [])
+    if (client) getUsers();
+  }, []);
+
+  if (error) {
+    return (
+      <ListContainer>
+        <div className="user-list__message">
+          Error loadin. Please refresh and try uno mas!
+        </div>
+      </ListContainer>
+    );
+  }
+
+  if (listEmpty) {
+    return (
+      <ListContainer>
+        <div className="user-list__message">
+          No users found.
+        </div>
+      </ListContainer>
+    );
+  }
 
   return (
     <ListContainer>
-      {loading ? <div className="user-list__message">Loading users ...</div> :(
+      {loading ? (
+        <div className="user-list__message">Loading users ...</div>
+      ) : (
         users?.map((user, i) => (
-            <UserItem index={i} key={user.id} user={user} setSelectedUsers={setSelectedUsers} />
+          <UserItem
+            index={i}
+            key={user.id}
+            user={user}
+            setSelectedUsers={setSelectedUsers}
+          />
         ))
       )}
       UserList
     </ListContainer>
-  )
-
-}
+  );
+};
 
 export default UserList;
