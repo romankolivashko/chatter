8096b4e3aac4165c1fdd28aff63d130c0ad22f90
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-21 10:52:51 -0800

    Added getChannels func to ChannelSearch

--- client/src/components/ChannelSearch.js
+++ client/src/components/ChannelSearch.js
@@ -4,12 +4,29 @@ import { getChannel, useChatContext } from "stream-chat-react";
 import { SearchIcon } from "../assets/search.png";
 
 const ChannelSearch = () => {
+  const { client, setActiveChannel } = useChatContext();
   const [query, setQuery] = useState("");
   const [loading, setLoading] = useState(false);
+  const [teamChannels, setTeamChannels] = useState([])
+  const [directChannels, setDirectChannels] = useState([])
 
   const getChannels = async (text) => {
     try {
-      //TODO: fetch channels
+      const channelResponse = client.queryChannels({
+          type: 'team', 
+          name: { $autocomplete: text}, 
+          memebers: { $in: [client.userID]}
+      });
+      const userResponse = client.queryUsers({
+        id: { $ne: client.userID },
+        name: { $autocomplete: text }
+      })
+
+      const [channels, { users }] = await Promise.all([channelResponse, userResponse]);
+
+      if(channels.length) setTeamChannels(channels);
+      if(users.length) setDirectChannels(users);
+
     } catch (error) {
       setQuery("");
     }
