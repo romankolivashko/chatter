e0f66f242e02d197f1d4669e979d3922a6db6852
Roman Kolivashko <rkolivashko@gmail.com>
2021-12-22 16:50:03 -0800

    Pre-deployment check, no issues

--- client/src/components/Auth.js
+++ client/src/components/Auth.js
@@ -27,7 +27,9 @@ const Auth = () => {
 
     const { username, password, phoneNumber, avatarURL } = form;
 
-    const URL = "http://localhost:5000/auth";
+    //const URL = "http://localhost:5000/auth";
+    const URL = "https://katchatapp.herokuapp.com/auth";
+
 
     const {
       data: { token, userId, hashedPassword, fullName },
